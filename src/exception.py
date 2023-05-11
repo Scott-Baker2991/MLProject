19c3d01e94a3f3ff6e2e5cb1d06f61984bf3fbb0
Scott-Baker2991 <scottbaker2991@gmail.com>
2023-05-11 17:49:51 +0100

    data_ingestion added

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,8 @@
 import sys
-import logger
-import logging
+from src.logger import logging
 
 
-def error_message_detail(error, error_detail:sys):
+def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info
     file_name=exc_tb.tb_frame.fcode.co_filename
     error_message='Error occured in Python script name [{0}] line number [{1}] error message [{2}]'.format(
@@ -14,12 +13,12 @@ def error_message_detail(error, error_detail:sys):
 
    
 
-    class CustomException(Exception):
-        def __init__(self,error_message,error_details:sys):
-            super().__init__(error_message)
-            self.error_message=error_message_detail(error_message,error_detail=error_detail)
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-        def __str__(self):
-            return self.error_message
+    def __str__(self):
+        return self.error_message
 
 
